0fc6804267fc525b4148a4d4a0b5ba986884ce8f
Nataliia333 <n.o.prykhodko@gmail.com>
2020-11-08 00:13:30 +0200

    task 6-7

---
+++ scripts/task-7.js
@@ -0,0 +1,14 @@
+/*Задача 6-7
+reduce
+Получи общую сумму баланса (сумму значений свойства balance) всех пользователей.
+
+Используй деструктурирующее присваивание для параметра функции {balance} без пробелов и переносов на новую строку
+
+Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив. Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы.*/
+// Write code under this line
+const calculateTotalBalance = users => {
+    return users.reduce((acc, user) => acc + user.balance, 0);
+};
+function calculateTotalBalance ({balance}) { return balance; }
+console.log(calculateTotalBalance(users)); 
+// 20916
